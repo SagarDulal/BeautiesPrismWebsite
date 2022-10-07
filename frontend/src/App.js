b04fc1d3636ed91fff00da59b64bc43c6ff15357
Sagar Dulal <sagardulal100@gmail.com>
2022-10-07 22:01:00 +0545

    Home Page component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 
 import Header from "./components/layouts/Header"
 import Footer from "./components/layouts/Footer"
+
+import Home from "./components/Home"
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer/>
     </div>
   );
